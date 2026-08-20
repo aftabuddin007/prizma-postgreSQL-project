24003cfad6846229eb8f27c5918fa03556c55f82
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-08-20 22:50:09 +0600

    add padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const [menuOpen,setMenuOpen]=useState(false)
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur ">
-      <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between py-4 px-4 sm:px-6 lg:px-8">
         {/* left section of navbar */}
         <div className="flex items-center space-x-4 md:space-x-6">
 <Link href={"/"} className='font-bold text-lg'>Food Cart</Link>
